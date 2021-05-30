c693e27d15992cb73700243df053460638340e3a
InAe Kim <deardata@naver.com>
2021-05-30 22:15:00 +0900

    포스트 상세 페이지에 카테고리 기능 추가하기

--- Django/blog/views.py
+++ Django/blog/views.py
@@ -1,6 +1,5 @@
 from .models import Post, Category
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render
 
 
 class PostList(ListView):
@@ -16,6 +15,13 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
 
 # Create your views here.
 # def index(request):
